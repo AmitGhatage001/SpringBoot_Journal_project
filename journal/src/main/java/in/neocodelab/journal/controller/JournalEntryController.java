a1b1d11a9d32131bb2a38c4a252b7ac7a77b061d
Amit Ghatage <amitghatage001@gmail.com>
2024-07-08 12:21:10 +0530

    Created hashMap for JournalEntry Class with <Long, JournalEntry> key value pairs in hashmap

--- journal/src/main/java/in/neocodelab/journal/controller/JournalEntryController.java
+++ journal/src/main/java/in/neocodelab/journal/controller/JournalEntryController.java
@@ -1,8 +1,19 @@
 package in.neocodelab.journal.controller;
 
+import in.neocodelab.journal.entity.JournalEntry;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 @RestController
 public class JournalEntryController {
+
+    private HashMap<Long, JournalEntry> journalEntries = new HashMap<>();
+
+    public List<JournalEntry> getAll(){
+        return new ArrayList<>(journalEntries.values());
+    }
 
 }
