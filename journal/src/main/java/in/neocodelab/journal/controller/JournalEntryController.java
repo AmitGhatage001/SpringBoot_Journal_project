0666515a9206f548ec3eaef40c1c68e3d88f346f
Amit Ghatage <amitghatage001@gmail.com>
2024-07-08 13:41:11 +0530

    Created a @PostMapping, Given @RequestBody to accept JSON, stored the json in JournalEntry POJO Object

--- journal/src/main/java/in/neocodelab/journal/controller/JournalEntryController.java
+++ journal/src/main/java/in/neocodelab/journal/controller/JournalEntryController.java
@@ -1,9 +1,7 @@
 package in.neocodelab.journal.controller;
 
 import in.neocodelab.journal.entity.JournalEntry;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -15,9 +13,15 @@ public class JournalEntryController {
 
     private HashMap<Long, JournalEntry> journalEntries = new HashMap<>();
 
-    @GetMapping("")
+    @GetMapping
     public List<JournalEntry> getAll(){
         return new ArrayList<>(journalEntries.values());
+    }
+
+    @PostMapping
+    public boolean createEntry(@RequestBody JournalEntry myEntry){
+        journalEntries.put(myEntry.getId(),myEntry);
+        return true;
     }
 
 }
