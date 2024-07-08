f2b5638c8c09c047cc657b8ec24e821aa5bfc2f5
Amit Ghatage <amitghatage001@gmail.com>
2024-07-08 11:49:11 +0530

    Initial Commit to remote repository

---
+++ journal/src/main/java/in/neocodelab/journal/JournalApplication.java
@@ -0,0 +1,15 @@
+package in.neocodelab.journal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JournalApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(JournalApplication.class, args);
+	}
+	// initial commit
+
+}
