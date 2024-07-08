7ad082584f92012fde09905ef4f61c2ba3f87b62
Amit Ghatage <amitghatage001@gmail.com>
2024-07-08 12:25:27 +0530

    added @RequestMapping annotation on Controller Class with path /journal

--- journal/src/main/java/in/neocodelab/journal/controller/JournalEntryController.java
+++ journal/src/main/java/in/neocodelab/journal/controller/JournalEntryController.java
@@ -1,6 +1,8 @@
 package in.neocodelab.journal.controller;
 
 import in.neocodelab.journal.entity.JournalEntry;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -8,10 +10,12 @@ import java.util.HashMap;
 import java.util.List;
 
 @RestController
+@RequestMapping("/journal")
 public class JournalEntryController {
 
     private HashMap<Long, JournalEntry> journalEntries = new HashMap<>();
 
+    @GetMapping("")
     public List<JournalEntry> getAll(){
         return new ArrayList<>(journalEntries.values());
     }
